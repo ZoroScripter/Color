612fdb59cfe421550de95804ba6eeb0828950ef2
ZoroScripter <87605900+ZoroScripter@users.noreply.github.com>
2021-08-03 18:14:20 -0300

    Update color.py

--- color.py
+++ color.py
@@ -19,47 +19,47 @@ print("\033[1;31m")
 os.system("figlet Color")
 
 print("""\033[1;32m
- Cores: \n\n
-l============l
-l "Verde"    l
-l "Amarelo"  l
-l "Vermelho" l
-l "Azul"     l
+ Cores: \n\n\033[1;37m
+l============l\033[1;32m
+l "Verde"    l\033[1;33m
+l "Amarelo"  l\033[1;31m
+l "Vermelho" l\033[1;34m
+l "Azul"     l\033[1;37m
 l============l
 """)
 cor = input(">>> ")
 
 if cor == "Verde":
-    os.chdir('/data/data/com.termux/files/home/Color/Bash/')
-    os.system('cp Verde /data/data/com.termux/files/usr/etc/')
-    os.chdir('/data/data/com.termux/files/usr/etc/')
-    os.system('rm -rf bash.bashrc')
-    os.system('mv Verde bash.bashrc')
-    print(f'{Vd}Pronto! \n\n')
-    print('Para salvar use o comando "exit" e volte ao terminal.')
+    os.chdir("/data/data/com.termux/files/home/Color/Bash/")
+    os.system("cp Verde /data/data/com.termux/files/usr/etc/")
+    os.chdir("/data/data/com.termux/files/usr/etc/')
+    os.system("rm -rf bash.bashrc")
+    os.system("mv Verde bash.bashrc")
+    print(f"{Vd}Pronto! \n\n")
+    print("Para salvar use o comando "exit" e volte ao terminal.")
 elif cor == "Amarelo":
-    os.chdir('/data/data/com.termux/files/home/Color/Bash/')
-    os.system('cp Amarelo /data/data/com.termux/files/usr/etc/')
-    os.chdir('/data/data/com.termux/files/usr/etc/')
-    os.system('rm -rf bash.bashrc')
-    os.system('mv Amarelo bash.bashrc')
-    print(f'\n{Vd}Pronto! \n\n')
-    print('Para salvar use o comando "exit" e volte ao terminal.')
+    os.chdir("/data/data/com.termux/files/home/Color/Bash/")
+    os.system("cp Amarelo /data/data/com.termux/files/usr/etc/")
+    os.chdir("/data/data/com.termux/files/usr/etc/")
+    os.system("rm -rf bash.bashrc")
+    os.system("mv Amarelo bash.bashrc")
+    print(f"\n{Vd}Pronto! \n\n")
+    print("Para salvar use o comando "exit" e volte ao terminal.")
 elif cor == "Vermelho":
-    os.chdir('/data/data/com.termux/files/home/Color/Bash/')
-    os.system('cp Vermelho /data/data/com.termux/files/usr/etc/')
-    os.chdir('/data/data/com.termux/files/usr/etc/')
-    os.system('rm -rf bash.bashrc')
-    os.system('mv Vermelho bash.bashrc')
-    print(f'{Vd}Pronto! \n\n')
-    print('Para salvar use o comando "exit" e volte ao terminal.')
+    os.chdir("/data/data/com.termux/files/home/Color/Bash/")
+    os.system("cp Vermelho /data/data/com.termux/files/usr/etc/")
+    os.chdir("/data/data/com.termux/files/usr/etc/")
+    os.system("rm -rf bash.bashrc")
+    os.system("mv Vermelho bash.bashrc")
+    print(f"{Vd}Pronto! \n\n")
+    print("Para salvar use o comando "exit" e volte ao terminal.")
 elif cor == "Azul":
-    os.chdir('/data/data/com.termux/files/home/Color/Bash/')
-    os.system('cp Azul /data/data/com.termux/files/usr/etc/')
-    os.chdir('/data/data/com.termux/files/usr/etc/')
-    os.system('rm -rf bash.bashrc')
-    os.system('mv Azul bash.bashrc')
-    print(f'\n{Vd}Pronto! \n\n')
-    print('Para salvar use o comando "exit" e volte ao terminal.')
+    os.chdir("/data/data/com.termux/files/home/Color/Bash/")
+    os.system("cp Azul /data/data/com.termux/files/usr/etc/")
+    os.chdir("/data/data/com.termux/files/usr/etc/")
+    os.system("rm -rf bash.bashrc")
+    os.system("mv Azul bash.bashrc")
+    print(f"\n{Vd}Pronto! \n\n")
+    print("Para salvar use o comando "exit" e volte ao terminal.")
 else:
     os.system("python3 color.py")
